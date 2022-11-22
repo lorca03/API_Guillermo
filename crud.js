992d6f1a4b37d0b1244445bd0aa0f6d5faef6e5b
lorca03 <guillermolorcamartinez@gmail.com>
2022-11-22 13:39:24 +0100

    api

---
+++ crud.js
@@ -0,0 +1,24 @@
+
+
+const añadir = document.getElementById("añadirUsuario");
+
+let usuario={
+    name:document.getElementById("nombre"),
+    email:document.getElementById("email"),
+    password:document.getElementById("password")
+}
+//const confirmar = document.getElementById("confirmar");
+
+async function peticionAñadir() {
+    
+    console.log(usuario)
+   fetch(`http://localhost:8000/api/user/`,{
+    method:'POST',
+    body:JSON.stringify(usuario)})
+    .then(response=>response.json())
+    .then(correcto=>{console.log(correcto)});
+    console.log(usuario)
+}
+
+// eslint-disable-next-line no-unused-vars
+añadir.addEventListener("click", ()=>peticionAñadir());
---
+++ server.js
@@ -0,0 +1,122 @@
+// Crear aplicación express 
+var express = require("express") 
+var app = express() 
+var db = require("./database.js")
+var md5 = require("md5")
+
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Puerto del servidor 
+var HTTP_PORT = 8000 
+// Iniciar servidor 
+app.listen ( HTTP_PORT , () => { 
+    console.log("Servidor en ejecución en el puerto %PORT%".replace("%PORT%",HTTP_PORT)) 
+}); 
+// Punto final raíz 
+app.get ("/", (req, res, next) => { 
+    res.json({"message":"success"}) 
+}); 
+// Inserte aquí otros puntos finales de la API 
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params =[data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
+app.patch("/api/user/:id", (req, res, next) => {
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : req.body.password ? md5(req.body.password) : null
+    }
+    db.run(
+        `UPDATE user set 
+           name = COALESCE(?,name), 
+           email = COALESCE(?,email), 
+           password = COALESCE(?,password) 
+           WHERE id = ?`,
+        [data.name, data.email, data.password, req.params.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
+})
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM user WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", changes: this.changes})
+    });
+})
+// Respuesta predeterminada para cualquier otra solicitud 
+app.use (function(req, res){ 
+    res.status(404); 
+});
+
