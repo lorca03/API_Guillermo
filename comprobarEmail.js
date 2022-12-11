829a29668909492820241065f2b98056a85f65fd
lorca03 <guillermolorcamartinez@gmail.com>
2022-12-11 23:49:36 +0100

    anotaciones

--- comprobarEmail.js
+++ comprobarEmail.js
@@ -1,18 +1,37 @@
 const formAnotacion = document.getElementById("formAnotacion");
-async function comprobarEmail() {
-  const email=  document.getElementById("emailAnotacion").value;
-  alert(email)
-  const miFeth = await fetch("https://localhost:8080/api/emails");
-  const json = await miFeth.json();
-  for (let index = 0; index < json.data.length; index++) {
-    if (email==json.data[index]) {
-        alert('El email existe')
+const boton = document.getElementById("submiranotacion");
+const email = document.getElementById("emailAnotacion").value;
+function comprobarEmail() {
+  alert(email);
+  fetch("https://localhost:8080/api/emails", {
+    method: "get",
+    headers: {
+        "Content-Type": "text/plain"
     }
-    
-  }
+}).then(function(response) {
+    return response.json();
+  });
+}
+function hacerPeticion () {
+  fetch("https://localhost:8080/api/emails", {
+    method: "get"
+}).then(function(response) {
+    return response.json();
+  });
+}
+
+function comprobarEmail() {
+  var json= hacerPeticion()
+  console.log(json)
 }
 formAnotacion.addEventListener("submit", (e) => {
   e.preventDefault();
-  comprobarEmail()
+  boton.addEventListener('click',comprobarEmail())
+  //comprobarEmail();
+  /*for (let index = 0; index < json.data.length; index++) {
+    if (email == json.data[index]) {
+      alert("El email existe");
+    }
+  }*/
   //formAnotacion.submit();
 });
